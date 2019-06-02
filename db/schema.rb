69aa2e8f0f543b1c03128738da59b192ef24feb6
sunder <jsundervivek1996@gmail.com>
2019-06-02 16:20:26 +0530

    'sort-field-tag'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_054752) do
+ActiveRecord::Schema.define(version: 2019_06_02_091626) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
